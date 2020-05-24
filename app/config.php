e9e75e6b8a91811a2b170c805bd51fd2aa7c7088
anav238 <45420849+anav238@users.noreply.github.com>
2020-05-24 12:38:57 +0300

    Delete config.php

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-putenv("IMG_PATH=/public/assets/img");
-putenv("CSS_PATH=/public/assets/css");
-putenv("JS_PATH=/public/assets/js");
-putenv("GITHUB_CLIENT_ID=a6e268aff8385be65501");
-putenv("GITHUB_CLIENT_SECRET=5158e6756eb7b098fce17cf53f20d02085a1ff13");
-putenv("DB_USER=xoopvbchaakxnc");
-putenv("DB_PASS=147a6ba6fcc381842a5724307e9c632a1e95d0e2e9fabeda4d11a1b1d697d4e3");
